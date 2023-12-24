36c3876132a2dbfe4083b543a27ff7ae901257f2
darshithv <darshithv3392@gmail.com>
2023-12-24 21:16:08 +0530

    added new files

---
+++ JAVASCRIPT/Assignments/Assignment-06/exercise.js
@@ -0,0 +1,31 @@
+function gamingButton(){
+  const button = document.querySelector('.js-toggled-button');
+
+  if(!button.classList.contains('is-toggled')){
+    button.classList.add('is-toggled');
+  }else{
+    button.classList.remove('is-toggled');
+  }
+}
+
+function toggledButton(selector){
+  const _toggled = document.querySelector(selector);
+
+  if(!_toggled.classList.contains('is-toggled')){
+
+    // before turning this button ON, check if there's already a button that's turned ON and turn it OFF
+    turnOffPreviousButton();
+
+    _toggled.classList.add('is-toggled');
+  } else {
+    _toggled.classList.remove('is-toggled');
+  }
+}
+
+function turnOffPreviousButton(){
+  const previousButton = document.querySelector('.is-toggled');
+
+  if(previousButton){
+    previousButton.classList.remove('is-toggled');
+  }
+}
