59ecebb460fce212552beae4ec00e61ad9e1d354
darshithv <darshithv3392@gmail.com>
2024-01-04 21:34:26 +0530

    modified file

--- JAVASCRIPT/Assignments/Assignment-08/exercise.js
+++ JAVASCRIPT/Assignments/Assignment-08/exercise.js
@@ -1,11 +1,14 @@
+/*
 // 1st question
-let words = ['hello', 'world', 'search', 'good'];
+
+let words = ['hello', 'world', 'search', 'good','search'];
 
 let index = -1;
 
 for (let i = 0; i < words.length; i++){
   if (words[i] === 'search') {
     index = i;
+    break;
   }
 }
 
@@ -22,6 +25,22 @@ for (let i = 0; i < words.length; i++){
 }
 
 console.log(index);
+*/
 
 // 2nd question
+
+function findIndex(array, word) {
+  let index = -1;
+
+  for (let i = 0; i < array.length; i++){
+    if (array[i] === word) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
+
+console.log(findIndex(['green', 'red', 'blue', 'red'], 'red'));
+console.log(findIndex(['green', 'red', 'blue', 'red'], 'yellow'));
 
